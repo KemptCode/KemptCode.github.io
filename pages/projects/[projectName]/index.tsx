809d68d767b2dd182c128d73a6ddded4c05e84ec
KemptCode <bar19026@byui.edu>
2023-07-04 07:50:02 -0700

    JSON to data folder

--- pages/projects/[projectName]/index.tsx
+++ pages/projects/[projectName]/index.tsx
@@ -7,43 +7,6 @@ import Footer from '@/components/footer/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
-const lookupTable: {
-  [name: string]: {
-    title: string,
-    endTimeStamp: Date,
-    startTimeStamp?: Date,
-    tags: string[],
-    thumbnail: string,
-    description: string,
-    content: any
-  }
-} = {
-  'spleef': {
-    title: 'Spleef in Roblox',
-    endTimeStamp: new Date(2015, 2, 6),
-    tags: ['lua', 'roblox', 'game'],
-    thumbnail: '',
-    description: 'Something about the old project',
-    content: <></>
-  },
-  'mazeGenerator': {
-    title: 'Maze Generator',
-    endTimeStamp: new Date(2020, 10, 20),
-    tags: ['python', 'algorithm'],
-    thumbnail: 'https://github.com/KemptCode/Maze_generator/raw/master/maze_5.png',
-    description: 'Creates a maze using a Prim\s algorithm.',
-    content: <></>
-  },
-  'fingerTracker': {
-    title: 'Finger Tracker Game',
-    endTimeStamp: new Date(2021, 4, 5),
-    tags: ['java', 'ml', 'computer vision', 'game'],
-    thumbnail: '',
-    description: 'Uses MediaPipe to track fingers in a gamified way.',
-    content: <></>
-  },
-};
-
 export default function Home() {
   const router = useRouter();
 
@@ -57,7 +20,7 @@ export default function Home() {
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
         <Navbar />
-        {lookupTable[router.query.projectName?.toString() ?? '']}
+        
         <Footer />
       </main>
     </>
